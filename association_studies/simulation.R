15b37d4a5ac419caa65f02ddaceb6a68d7b5ee1d
Leslie Myint <leslie.myint@gmail.com>
2017-02-09 00:01:28 -0500

    Try out new simulations. Move to Rmd file for class

--- association_studies/simulation.R
+++ association_studies/simulation.R
@@ -7,97 +7,100 @@ library(scales)
 ## numZero: number of predictors whose coefficients are zero (not intercept)
 ## numObs: total number of observations
 simulateRegressionData <- function(numPredictors, numZero = 0, numObs = 1000, numDatasets = 1000, equalVar = TRUE) {
-	coeffs <- runif(numPredictors+1-numZero, 0, 5)
-	coeffs <- c(coeffs, rep(0, numZero))
 	datasets <- lapply(seq_len(numDatasets), function(i) {
+		## Simulate coefficients
+		coeffs <- runif(numPredictors+1-numZero, 0, 5)
+		coeffs <- c(coeffs, rep(0, numZero))
+		names(coeffs) <- c("intercept", paste0("x", seq_len(numPredictors)))
 		## Make a numObs x numPredictors matrix
 		predictors <- do.call(cbind, lapply(seq_len(numPredictors), function(j) {
-			rt(numObs, df = 1, ncp = 10)
+			rt(numObs, df = 10, ncp = 10)
 		}))
 		## Multiply predictors matrix by coefficients
 		outcome <- rowSums(sweep(predictors, 2, tail(coeffs, -1), FUN = "*")) + coeffs[1]
 		if (equalVar) {
 			outcome <- outcome + rnorm(numObs, 0, 5)
 		} else {
-			outcome <- outcome + rnorm(numObs, 0, outcome)
+			outcome <- outcome + rnorm(numObs, 0, 10*((outcome/max(outcome))+0.1)^2)
 		}
 		df <- as.data.frame(cbind(outcome, predictors))
 		colnames(df) <- c("y", paste0("x", seq_len(numPredictors)))
+		attr(df, "trueCoeffs") <- coeffs
 		return(df)
 	})
-	names(coeffs) <- c("(Intercept)", paste0("x", seq_len(numPredictors)))
-	attr(datasets, "trueCoeffs") <- coeffs
+	attr(datasets, "equalVar") <- equalVar
 	return(datasets)
 }
 
-fitModels <- function(datasets, dropLowest = 0, dropHighest = 0) {
-	coeffs <- tail(attr(datasets, "trueCoeffs"), -1)
-	numCoeffs <- length(coeffs)
+fitModels <- function(datasets, weighted = FALSE, dropLowest = 0, dropHighest = 0) {
+	numCoeffs <- ncol(datasets[[1]])-1
 	if (dropLowest + dropHighest >= numCoeffs) {
 		stop("Need to keep at least one predictor")
 	}
-	if (dropLowest > 0) {
-		whichDropLow <- order(coeffs)[1:dropLowest]
-	} else {
-		whichDropLow <- c()
-	}
-	if (dropHighest > 0) {
-		whichDropHigh <- order(coeffs)[(numCoeffs-dropHighest+1):numCoeffs]
-	} else {
-		whichDropHigh <- c()
-	}
-	dropCols <- c(whichDropLow, whichDropHigh)
-	keepCols <- c(1, setdiff(seq_len(numCoeffs), dropCols)+1)
-	print(keepCols)
 	fits <- lapply(seq_along(datasets), function(i) {
 		df <- datasets[[i]]
+		coeffs <- attr(df, "trueCoeffs")
+		if (dropLowest > 0) {
+			whichDropLow <- order(coeffs)[1:dropLowest]
+		} else {
+			whichDropLow <- c()
+		}
+		if (dropHighest > 0) {
+			whichDropHigh <- order(coeffs)[(numCoeffs-dropHighest+1):numCoeffs]
+		} else {
+			whichDropHigh <- c()
+		}
+		dropCols <- c(whichDropLow, whichDropHigh)
+		keepCols <- c(1, setdiff(seq_len(numCoeffs), dropCols)+1)
+		## Subset data
 		df <- df[,keepCols]
 		lmfit <- lm(y ~ ., data = df)
+		if (weighted) {
+			res <- residuals(lmfit)
+			ycut <- as.numeric(cut(df$y, breaks = 30))
+			ressd <- tapply(res, ycut, sd)
+			w <- 1/ressd^2
+			w <- w[ycut]
+			lmfit <- lm(y ~ ., data = df, weights = w)
+		}
+		attr(lmfit, "trueCoeffs") <- coeffs[keepCols]
 		return(lmfit)
 	})
-	attr(fits, "trueCoeffs") <- attr(datasets, "trueCoeffs")[keepCols]
 	return(fits)
 }
 
-plotResults <- function(fits, ...) {
-	trueCoeffs <- attr(fits, "trueCoeffs")
-	coeffMat <- do.call(cbind, lapply(fits, function(fit) {
-		coefficients(fit)
-	}))
-	par(mfrow = c(1,2))
-	matplot(x = seq_len(nrow(coeffMat)), y = coeffMat, pch = 16, col = alpha("black", 0.1), xlab = "", ylab = "Coefficient estimate", xaxt = "n", ...)
-	points(x = seq_len(nrow(coeffMat)), y = trueCoeffs, col = "red")
-	axis(side = 1, at = seq_len(nrow(coeffMat)), labels = rownames(coeffMat))
-
-	coeffList <- lapply(seq_len(nrow(coeffMat)), function(i) {
-		coeffMat[i,]
+summarizeResults <- function(fits, ...) {
+	rmse <- sapply(fits, function(fit) {
+		trueCoeffs <- attr(fit, "trueCoeffs")
+		sqrt(mean((coefficients(fit)-trueCoeffs)^2))
 	})
-	boxplot(coeffList, outline = FALSE, xaxt = "n", ...)
-	points(x = seq_len(nrow(coeffMat)), y = trueCoeffs, col = "red")
-	axis(side = 1, at = seq_len(nrow(coeffMat)), labels = rownames(coeffMat))
-}
-
-accuracyMeasures <- function(fits) {
-	trueCoeffs <- attr(fits, "trueCoeffs")
-	medianAbsError <- sapply(fits, function(fit) {
-		median(abs(coefficients(fit)-trueCoeffs))
-	})
-	return(medianAbsError)
+	plot(density(rmse, from = 0), xlab = "Square root(mean squared error)", ...)
+	invisible(rmse)
 }
 
 set.seed(1)
-simdata <- simulateRegressionData(numPredictors = 5, numZero = 1, numObs = 1000, numDatasets = 1000)
-simdataHetero <- simulateRegressionData(numPredictors = 5, numZero = 1, numObs = 1000, numDatasets = 1000, equalVar = FALSE)
+simdata <- simulateRegressionData(numPredictors = 15, numZero = 10, numObs = 1000, numDatasets = 1000)
 fits <- fitModels(simdata)
-fitsHetero <- fitModels(simdataHetero)
 fits2 <- fitModels(simdata, dropHighest = 1)
 fits3 <- fitModels(simdata, dropLowest = 1)
 fits4 <- fitModels(simdata, dropLowest = 2)
 plotResults(fits, main = "Drop none")
-plotResults(fitsHetero, main = "Drop none (hetero)", ylim = c(-5,5))
 plotResults(fits2, main = "Drop highest (1)")
 plotResults(fits3, main = "Drop lowest (1)")
 plotResults(fits4, main = "Drop lowest (2)")
+
+simdataHetero <- simulateRegressionData(numPredictors = 1, numZero = 0, numObs = 1000, numDatasets = 1000, equalVar = FALSE)
+fitsHeteroNoWeight <- fitModels(simdataHetero)
+par(mfrow = c(2,2))
+plot(fitsHeteroNoWeight[[1]])
+fitsHeteroWeight <- fitModels(simdataHetero, weighted = TRUE)
+plot(fitsHeteroWeight[[1]])
+par(mfrow = c(1,2))
+accnow <- summarizeResults(fitsHeteroNoWeight)
+accw <- summarizeResults(fitsHeteroWeight)
+boxplot(accnow, accw, outline = FALSE)
+
+
 acc <- accuracyMeasures(fits)
 acc2 <- accuracyMeasures(fits2)
 acc3 <- accuracyMeasures(fits3)
@@ -106,8 +109,24 @@ acc4 <- accuracyMeasures(fits4)
 boxplot(acc, acc2, acc3, acc4, outline = FALSE, ylim = c(0,0.01))
 boxplot(acc, acc2, acc3, acc4, outline = FALSE)
 
+par(mfrow = c(2,5))
+for (i in 10:1) {
+	simdata <- simulateRegressionData(numPredictors = 15, numZero = i, numObs = 1000, numDatasets = 500)
+	fits <- fitModels(simdata)
+	plotResults(fits, main = paste("# zero:", i))
+}
 
-
-
+x = rnorm(1000)
+y = 3 + 2*x + rnorm(1000, 0, abs(x))
+df <- data.frame(y = y, x = x)
+lmfit <- lm(y ~ x, data = df)
+par(mfrow = c(2,2))
+plot(lmfit)
+summary(lmfit)
+w <- 1/abs(x)
+lmfitw <- lm(y ~ x, data = df, weights = w)
+par(mfrow = c(2,2))
+plot(lmfitw)
+summary(lmfitw)
 
 
